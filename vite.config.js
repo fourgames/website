24f971af69dff37ebdc31caeec7fc0fd315a07c7
danny-karlsson-ju <ytdannykarlsson@gmail.com>
2026-03-24 22:10:44 +0100

    disable purgecss but good test 20x
    683,5 kB to 34,4 kB

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import purgecss from "vite-plugin-purgecss";
 
 export default defineConfig({
 	plugins: [
-		purgecss({
-			content: ["./index.html", "./src/**/*.{js,ts}", "./main.js"],
-			variables: true,
-			keyframes: true,
-			// safelist: { standard: [/is-/, /has-/] } <-- Testa att ta bort denna tillfälligt
-		}),
+		// purgecss({
+		// 	content: ["./index.html", "./src/**/*.{js,ts}", "./main.js"],
+		// 	variables: true,
+		// 	keyframes: true,
+		// 	// safelist: { standard: [/is-/, /has-/] } <-- Testa att ta bort denna tillfälligt
+		// }),
 	],
 });

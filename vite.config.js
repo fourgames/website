23d35726c5bf90bde108917fafc981972ab96ad6
danny-karlsson-ju <ytdannykarlsson@gmail.com>
2026-03-24 21:57:20 +0100

    purgecss test

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import purgecss from "vite-plugin-purgecss";
+
+export default defineConfig({
+	plugins: [
+		purgecss({
+			content: ["./index.html", "./src/**/*.{js,ts}", "./main.js"],
+			variables: true,
+			keyframes: true,
+			// safelist: { standard: [/is-/, /has-/] } <-- Testa att ta bort denna tillfälligt
+		}),
+	],
+});

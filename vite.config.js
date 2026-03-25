60ca725ff674007ad2fea935f0ae9976b7da2da3
danny-karlsson-ju <ytdannykarlsson@gmail.com>
2026-03-25 21:30:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import purgecss from "vite-plugin-purgecss";
 
 export default defineConfig({
-  base: './', // Detta gör alla sökvägar relativa, vilket fungerar överallt
-  // ... dina andra inställningar
+	base: "/website/", // Detta gör alla sökvägar relativa, vilket fungerar överallt
+	// ... dina andra inställningar
 	plugins: [
 		// purgecss({
 		// 	content: ["./index.html", "./src/**/*.{js,ts}", "./main.js"],

e1d32c1e08ff2cc2e6aee6cfa3606d1c90c913b9
danny-karlsson-ju <ytdannykarlsson@gmail.com>
2026-03-25 21:40:41 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,18 @@ import { defineConfig } from "vite";
 import purgecss from "vite-plugin-purgecss";
 
 export default defineConfig({
-	base: "/", // Detta gör alla sökvägar relativa, vilket fungerar överallt
-	// ... dina andra inställningar
-	plugins: [
-		// purgecss({
-		// 	content: ["./index.html", "./src/**/*.{js,ts}", "./main.js"],
-		// 	variables: true,
-		// 	keyframes: true,
-		// 	// safelist: { standard: [/is-/, /has-/] } <-- Testa att ta bort denna tillfälligt
-		// }),
-	],
+	base: "/", // VIKTIGT: Bara ett snedstreck här!
 });
+
+// export default defineConfig({
+// 	base: "/", // Detta gör alla sökvägar relativa, vilket fungerar överallt
+// 	// ... dina andra inställningar
+// 	plugins: [
+// 		// purgecss({
+// 		// 	content: ["./index.html", "./src/**/*.{js,ts}", "./main.js"],
+// 		// 	variables: true,
+// 		// 	keyframes: true,
+// 		// 	// safelist: { standard: [/is-/, /has-/] } <-- Testa att ta bort denna tillfälligt
+// 		// }),
+// 	],
+// });

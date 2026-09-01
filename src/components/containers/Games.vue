1f49ab46757dd46fc43682b2518c7d188d510265
danny-karlsson-ju <ytdannykarlsson@gmail.com>
2026-09-01 21:41:48 +0200

    add my other appid's

--- src/components/containers/Games.vue
+++ src/components/containers/Games.vue
@@ -1,14 +1,16 @@
 <script setup>
 import ContainerWrapper from "../common/ContainerWrapper.vue";
+
+const steamAppIds = [2807130, 3958210, 5207970];
 </script>
 
 <template>
 	<ContainerWrapper title="Our Games">
 		<div class="grid grid-cols-1 md:grid-cols-2 gap-0">
-			<div v-for="n in 2" :key="n" class="w-full p-2">
+			<div v-for="appId in steamAppIds" :key="appId" class="w-full p-2">
 				<div class="overflow-hidden rounded-lg shadow-none">
 					<iframe
-						src="https://store.steampowered.com/widget/2807130/"
+						:src="`https://store.steampowered.com/widget/${appId}/`"
 						frameborder="0"
 						class="w-full h-[190px]"
 						style="color-scheme: light"
